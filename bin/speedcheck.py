48e10b5c9208f17b84181e4b53e7080cee29bc39
f8al <f8al.err0r@gmail.com>
2017-05-09 13:27:35 -0700

    migration to twitter from email

--- bin/speedcheck.py
+++ bin/speedcheck.py
@@ -1,16 +1,12 @@
 #!/bin/python
 import ConfigParser
-import os
 import speedtest
-import json
-
-
-
-
+import tweepy
 
 #load configuration file
 Config = ConfigParser.ConfigParser()
 Config.read("../etc/speedcheck.conf")
+
 
 print Config
 
@@ -29,25 +25,47 @@ def ConfigSectionMap(section):
     return dict1
 
 
-#HOST = ConfigSectionMap("global")['lhost']
-#PORT = ConfigSectionMap("global")['lport']
 
 
-servers = []
-# If you want to test against a specific server
-# servers = [1234]
 
-s = speedtest.Speedtest()
-s.get_servers(servers)
-s.get_best_server()
-s.download()
-s.upload()
+def s_test():
+    #servers = []
+    # If you want to test against a specific server
+    # servers = [1234]
+    s = speedtest.Speedtest()
+    s.get_servers(servers)
+    s.get_best_server()
+    s.download()
+    s.upload()
+    results_dict = s.results.dict()
+    print "Download speed is: " + str(results_dict["download"]) + "!"
+    print "Upload speed is: " + str(results_dict["upload"]) + "!"
+    print "Ping is: " + str(results_dict["ping"]) + "!"
 
 
-results_dict = s.results.dict()
 
-print results_dict
 
-output = json.loads({results_dict}, sort_keys=True, indent=4, seperators=(',', ': '))
 
-print output
+
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+def main():
+  # Fill in the values noted in previous step here
+  cfg = {
+    "consumer_key"        : "VALUE",
+    "consumer_secret"     : "VALUE",
+    "access_token"        : "VALUE",
+    "access_token_secret" : "VALUE"
+    }
+
+  api = get_api(cfg)
+  tweet = "Hello, world!"
+  status = api.update_status(status=tweet)
+  # Yes, tweet is called 'status' rather confusing
+
+if __name__ == "__main__":
+  main()
+
