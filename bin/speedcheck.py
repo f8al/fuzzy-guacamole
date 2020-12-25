c0b4fc103916ca623f522eca23c3991897177a13
f8al <f8al.err0r@gmail.com>
2020-12-24 18:11:50 -0600

    Updating for python3.x compatibility

--- bin/speedcheck.py
+++ bin/speedcheck.py
@@ -2,48 +2,63 @@
 import speedtest
 import tweepy
 import requests
-from ConfigParser import SafeConfigParser
-import json
+import configparser
+import os
+import logging
 
+
+pwd = os.getcwd()
 
 #load configuration file
-parser = SafeConfigParser()
-parser.read('../etc/guac.conf') #change this to speedtest.conf once configured
+parser = configparser.ConfigParser()
+config = pwd + '/etc/speedtest.conf'
+parser.read( config ) #change this to speedtest.conf once configured
+
 
 def get_api(cfg):
-  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
-  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
-  return tweepy.API(auth)
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logging.error("Error creating API", exc_info=True)
+        raise e
+    logging.info("API created")
+    return api
 
 
 def main():
     '''get configs'''
-    c_key = parser.get('twitter', 'consumer_key')
-    c_secret = parser.get('twitter', 'consumer_secret')
-    a_token = parser.get('twitter', 'access_token')
-    a_t_secret = parser.get('twitter','access_token_secret')
-    handle = parser.get('twitter', 'isp_handle')
-    down = parser.get('global','speed_down')
-    up = parser.get('global', 'speed_up')
-    HEC_token = parser.get('splunk', 'HEC_token')
-    splunk_server = parser.get('splunk', 'splunk_server')
-    splunk_enabled = parser.get('splunk', 'splunk')
+    c_key = parser['twitter']['consumer_key']
+    c_secret = parser['twitter']['consumer_secret']
+    a_token = parser['twitter']['access_token']
+    a_t_secret = parser['twitter']['access_token_secret']
+    handle = parser['twitter']['isp_handle']
+    down = parser['global']['speed_down']
+    up = parser['global']['speed_up']
 
     '''SPEED TEST'''
     servers = []
+    threads = 10
     s = speedtest.Speedtest()
     s.get_servers(servers)
     s.get_best_server()
-    s.download()
-    s.upload()
-    results_dict = s.results.dict()
+    s.download(threads=threads)
+    s.upload(threads=threads, pre_allocate=False)
+    s.results.share()
+    results = s.results.dict()
 
-    dl = float(results_dict["download"])
-    ul = float(results_dict["upload"])
+    dl = float(results["download"])
+    ul = float(results["upload"])
     dl = ((dl / 1024) / 1024)
     ul = ((ul / 1024) / 1024)
-    dlr = round(dl,2)
-    ulr = round(ul,2)
+    dl = round(dl,2)
+    ul = round(ul,2)
+    down = parser['global']['speed_down']
+    up = parser['global']['speed_up']
 
     cfg = {
         "consumer_key"        : c_key,
@@ -52,26 +67,16 @@ def main():
         "access_token_secret" : a_t_secret
     }
     api = get_api(cfg)
-    if float(dlr) <= float(down) * 0.66 and float(ulr) <= float(up) * 0.66:
-        tweet = "hey " + handle + " fuzzy-guacamole says current download speed is " + str(dlr) + "Mbps & upload is " + str(ulr) + "Mbps! This is < 66% of paid service level! #fuzzyguac"
-    elif float(dlr) <= float(down) * 0.66:
-        tweet = "hey "+ handle +" fuzzy-guacamole says current download speed is " + str(dlr) + " Mbps! This is less than 66% of paid service level! #shame #fuzzyguac"
-    elif float(ulr) <= float(up) * 0.66:
-        tweet = "hey "+ handle +" fuzzy-guacamole says current upload speed is " + str(ulr) + " Mbps! This is less than 66% of paid service level! #shame #fuzzyguac"
+    if float(dl) <= float(down) * 0.66 and float(ul) <= float(up) * 0.66:
+        tweet = f"hey {handle} fuzzy-guacamole says current download speed is {dl} Mbps & upload is {ul} Mbps! This is < 66% of paid service level! #fuzzyguac"
+    elif float(dl) <= float(down) * 0.66:
+        tweet = f"hey {handle} fuzzy-guacamole says current download speed is {dl} Mbps! This is less than 66% of paid service level! #shame #fuzzyguac"
+    elif float(ul) <= float(up) * 0.66:
+        tweet = f"hey {handle} fuzzy-guacamole says current upload speed is {ul} Mbps! This is less than 66% of paid service level! #shame #fuzzyguac"
     else:
         exit()
-    print tweet
-    status = api.update_status(status=tweet)
-
-
-    if splunk_enabled == "True":
-        headers = {'Authorization' : HEC_token}
-        payload = str(results_dict)
-        payload = json.dumps(payload)
-        payload = '{ "event": ' + payload + '}\r\n'
-        print payload
-        r = requests.post('https://'+ splunk_server + ':8088/services/collector/event', headers=headers, data=payload, verify=False)
-        print r.text
+    print(tweet)
+    api.update_status(status=tweet)
 
 if __name__ == "__main__":
   main()
